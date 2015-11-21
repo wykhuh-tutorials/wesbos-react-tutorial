05e2088f7b98a8117c2ce56431b105c56b803f7e
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-21 11:19:06 -0800

    6 add props to header for dynamic tagline

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,11 @@ var ReactDOM = require('react-dom');
 
 var App = React.createClass({
   render: function() {
+    // pass in tagline as a prop to Header Component
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
         <Inventory />
@@ -19,7 +20,13 @@ var Header = React.createClass({
   render: function() {
     return (
       <header className="top">
-        <h1>Catch of the Day</h1>
+        <h1>Catch
+           <span className="ofThe">
+             <span className="of">of</span>
+             <span className="the">the</span>
+           </span>
+         Day</h1>
+       <h3 className="tagline"><span>{this.props.tagline}</span></h3>
       </header>
     )
   }
