114622f7861b687c2ec538320cec8cc0f90114e2
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-21 15:03:50 -0800

    13 show "add to order" or "Sold out" button

--- scripts/main.js
+++ scripts/main.js
@@ -79,6 +79,8 @@ var App = React.createClass({
 var Fish = React.createClass({
   render: function() {
     var details = this.props.details;
+    var isAvailable = (details.status === 'available'? true : false);
+    var buttonText = (isAvailable ? 'Add To Order' : 'Sold Out!')
     return (
       <li className="menu-fish">
         <img src="{details.image}" alt="{details.name}" />
@@ -87,6 +89,7 @@ var Fish = React.createClass({
           <span className="price">{helpers.formatPrice(details.price)}</span>
         </h3>
         <p>{details.desc}</p>
+        <button disabled={!isAvailable}>{buttonText}</button>
       </li>
     )
   }
