1d55bb167a3df32358b8a7d72327917adc7fb673
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-21 16:50:37 -0800

    14 display order total price

--- scripts/main.js
+++ scripts/main.js
@@ -78,7 +78,7 @@ var App = React.createClass({
             {Object.keys(this.state.fishes).map(this.renderFish)}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order}/>
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
@@ -167,8 +167,27 @@ var Header = React.createClass({
 
 var Order = React.createClass({
   render: function() {
+    var orderIds = Object.keys(this.props.order);
+    var total = orderIds.reduce((prevTotal, key) => {
+      var fish = this.props.fishes[key];
+      var count = this.props.order[key];
+      var isAvailable = fish && fish.status === 'available';
+
+      if (fish && isAvailable) {
+        return prevTotal + (count * parseInt(fish.price) || 0);
+      }
+
+      return prevTotal;
+    }, 0);
+
     return (
-      <p>Order</p>
+      <div className="order-wrap">
+        <h2 className="order-title">Your Order</h2>
+        <ul className="order">
+          <strong>Total:</strong>
+          {helpers.formatPrice(total)}
+        </ul>
+      </div>
     )
   }
 });
