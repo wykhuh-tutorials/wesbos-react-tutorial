b31298861a448b1f9a3d4088f271dae20fb1107a
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-22 08:42:58 -0600

    17 finish inventory form

--- scripts/main.js
+++ scripts/main.js
@@ -273,6 +273,13 @@ var Inventory = React.createClass({
     return (
       <div className="fish-edit" key={key}>
         <input type="text" valueLink={linkState('fishes.' + key + '.name')} />
+        <input type="text" valueLink={linkState('fishes.' + key + '.price')} />
+        <select valueLink={linkState('fishes.' + key + '.status')}>
+          <option value="unavailable">Sold Out</option>
+          <option value="available">Fresh</option>
+        </select>
+        <textarea valueLink={linkState('fishes.' + key + '.desc')}></textarea>
+        <input type="text" valueLink={linkState('fishes.' + key + '.image')} />
       </div>
     )
   },
