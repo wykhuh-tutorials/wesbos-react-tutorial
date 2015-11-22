0c2a3ce1d246f357d73ffb78cac6f73f6129da58
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-22 16:39:01 -0600

    clean up linting errors

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,6 @@ var ReactDOM = require('react-dom');
 var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
-var Navigation = ReactRouter.Navigation;
 var History = ReactRouter.History;
 // allows for html5 push state
 var createBrowserHistory = require('history/lib/createBrowserHistory');
@@ -17,7 +16,6 @@ var Rebase = require('re-base');
 // hook up to firebase. base is the reference to firebase.
 var base = Rebase.createClass('https://react-fish-huh.firebaseio.com/');
 
-
 var App = React.createClass({
   mixins: [Catalyst.LinkedStateMixin],
   // getInitialState is part of React life cycle.
@@ -27,7 +25,7 @@ var App = React.createClass({
     return {
       fishes: {},
       order: {}
-    }
+    };
   },
 
   // componentDidMount is part of React life cycle.
@@ -49,30 +47,30 @@ var App = React.createClass({
     base.syncState(
       this.props.params.storeId + '/fishes',
       // 'this' refer to App compontent
-      { context: this, state: 'fishes'}
+      { context: this, state: 'fishes' }
     );
 
     // grab data for current store from local storage
-    var localStorageRef = localStorage.getItem('order-' + this.props.params.storeId);
+    var localStorageRef =
+      localStorage.getItem('order-' + this.props.params.storeId);
     // set App order state to value in local storage
     if (localStorageRef) {
       this.setState({
         // local storage uses json. need to turn it into object
         order: JSON.parse(localStorageRef)
-      })
+      });
     }
   },
 
   // componentWillUpdate is part of React life cycle.
   // occurs when prop or state chamges
   componentWillUpdate: function(nextProps, nextState) {
-    console.log('next', nextState)
     // get store id from react router using this.props.params
     // local storage: key, value  as json. local storage uses json
     localStorage.setItem(
       'order-' + this.props.params.storeId,
        JSON.stringify(nextState.order)
-     )
+     );
   },
 
   // add one order to App state
@@ -102,7 +100,7 @@ var App = React.createClass({
   loadSamples: function() {
     this.setState({
       fishes: require('./sample-fishes')
-    })
+    });
   },
 
   // render a fish from App state
@@ -113,8 +111,11 @@ var App = React.createClass({
 
     // we can't access key property inside the component, so we have to create
     // an index property to access the unique key value
-    return <Fish key={key} index={key} details={this.state.fishes[key]}
-      addToOrder={this.addToOrder} />
+    return (
+      <Fish key={key} index={key} details={this.state.fishes[key]}
+        addToOrder={this.addToOrder} />
+    );
+
   },
 
   render: function() {
@@ -136,7 +137,7 @@ var App = React.createClass({
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}
           fishes={this.state.fishes} linkState={this.linkState}/>
       </div>
-    )
+    );
   }
 });
 
@@ -147,7 +148,7 @@ var Fish = React.createClass({
   render: function() {
     var details = this.props.details;
     var isAvailable = (details.status === 'available'? true : false);
-    var buttonText = (isAvailable ? 'Add To Order' : 'Sold Out!')
+    var buttonText = (isAvailable ? 'Add To Order' : 'Sold Out!');
     return (
       <li className="menu-fish">
         <img src={details.image} alt={details.name} />
@@ -156,9 +157,11 @@ var Fish = React.createClass({
           <span className="price">{helpers.formatPrice(details.price)}</span>
         </h3>
         <p>{details.desc}</p>
-        <button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
+        <button disabled={!isAvailable} onClick={this.onButtonClick}>
+          {buttonText}
+        </button>
       </li>
-    )
+    );
   }
 });
 
@@ -172,10 +175,8 @@ var AddFishForm = React.createClass({
       price: this.refs.price.value,
       status: this.refs.status.value,
       desc: this.refs.desc.value,
-      image: this.refs.image.value,
-    }
-    console.log(fish)
-
+      image: this.refs.image.value
+    };
     // add fish to App state
 
     // addFish is defined in <App />. We pass down addFish as prop from
@@ -198,11 +199,9 @@ var AddFishForm = React.createClass({
         <input type="text" ref="image" placeholder="URL to Image" />
         <button type="submit">+ Add Item </button>
       </form>
-    )
+    );
   }
 });
-
-
 
 var Header = React.createClass({
   render: function() {
@@ -216,7 +215,7 @@ var Header = React.createClass({
          Day</h1>
        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
       </header>
-    )
+    );
   }
 });
 
@@ -226,7 +225,9 @@ var Order = React.createClass({
     var count = this.props.order[key];
 
     if(!fish) {
-      return <li key={key}>Sorry, fish no longer available!</li>
+      return (
+        <li key={key}>Sorry, fish no longer available!</li>
+      );
     }
     return (
       <li key={key}>
@@ -234,7 +235,7 @@ var Order = React.createClass({
         {fish.name}
         <span className="price">{helpers.formatPrice(count * fish.price)}</span>
       </li>
-    )
+    );
   },
   render: function() {
     var orderIds = Object.keys(this.props.order);
@@ -261,10 +262,9 @@ var Order = React.createClass({
           </li>
         </ul>
       </div>
-    )
+    );
   }
 });
-
 
 var Inventory = React.createClass({
   renderInventory: function(key) {
@@ -281,7 +281,7 @@ var Inventory = React.createClass({
         <textarea valueLink={linkState('fishes.' + key + '.desc')}></textarea>
         <input type="text" valueLink={linkState('fishes.' + key + '.image')} />
       </div>
-    )
+    );
   },
   // instead of writing each prop, we can use {..this.props} to pass all props.
 
@@ -295,7 +295,7 @@ var Inventory = React.createClass({
         <AddFishForm {...this.props} />
         <button onClick={this.props.loadSamples}>Load Sample Fishes</button>
       </div>
-    )
+    );
   }
 });
 
@@ -313,7 +313,6 @@ var StorePicker = React.createClass({
 
     // this.refs.storeId is on object with lots of js and React properties
     var storeId = this.refs.storeId.value;
-    console.log(storeId);
 
     // transition from StorePicker to App
     this.history.pushState(null, '/store/' + storeId);
@@ -329,18 +328,21 @@ var StorePicker = React.createClass({
       <form className="store-selector" onSubmit={this.goToStore}>
         <p>Hi {name}.</p>
         <h2>Please enter a store</h2>
-        <input type="text" ref="storeId" defaultValue={helpers.getFunName()} required />
+        <input type="text" ref="storeId" defaultValue={helpers.getFunName()}
+          required />
         <input type="Submit" />
       </form>
-    )
+    );
   }
 });
 
 var NotFound = React.createClass({
   render: function() {
-    return <h1>Not Found</h1>
+    return (
+      <h1>Not Found</h1>
+    );
   }
-})
+});
 
 // Routes
 
@@ -352,8 +354,7 @@ var routes = (
     <Route path="/store/:storeId" component={App}/>
     <Route path="*" component={NotFound}/>
   </Router>
-)
-
+);
 
 // since React 1.4, to mount component on page, you need React DOM
 // render(component, html element)
